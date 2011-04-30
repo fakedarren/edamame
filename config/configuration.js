fdc5c96115009227f804105a39cc1293736ecfd9
fakedarren <darren.waddell@gmail.com>
2011-04-30 14:37:42 +0100

    Config changes for duostack

--- config/configuration.js
+++ config/configuration.js
@@ -1,5 +1,5 @@
 module.exports = {
-	host: 'http://node.local',
+	host: 'http://mootoolscms.duostack.net',
 	twitter: {
 		consumerKey: '1nUfwvkjGTkketskwt7wKA',
 		consumerSecret: 'TkJWEqxUCQfyU89lSwOw6D8keRp5FALKc7bq1UhoPnE'
