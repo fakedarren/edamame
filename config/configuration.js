eb23e7f120182ad0592e741f704a06980bcc6ad0
fakedarren <darren.waddell@gmail.com>
2011-05-15 18:31:02 +0100

    Config stuff

--- config/configuration.js
+++ config/configuration.js
@@ -1,6 +1,6 @@
 module.exports = {
-	database: DUOSTACK_DB_MONGODB,
-	host: 'http://mootoolscms.duostack.net/',
+	database: process.env['DUOSTACK_DB_MONGODB'],
+	host: 'http://mootoolscms.duostack.net',
 	twitter: {
 		consumerKey: '1nUfwvkjGTkketskwt7wKA',
 		consumerSecret: 'TkJWEqxUCQfyU89lSwOw6D8keRp5FALKc7bq1UhoPnE'
