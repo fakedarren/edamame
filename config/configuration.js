c81a512713a1afcfc63c32540538132c62ac104f
fakedarren <darren.waddell@gmail.com>
2011-05-15 18:58:20 +0100

    Failed experiment

--- config/configuration.js
+++ config/configuration.js
@@ -1,6 +1,6 @@
 module.exports = {
-	database: process.env['DUOSTACK_DB_MONGODB'],
-	host: 'http://mootoolscms.duostack.net',
+	database: 'mongodb://localhost/cms',
+	host: 'http://localhost',
 	twitter: {
 		consumerKey: '1nUfwvkjGTkketskwt7wKA',
 		consumerSecret: 'TkJWEqxUCQfyU89lSwOw6D8keRp5FALKc7bq1UhoPnE'
