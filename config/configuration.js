217f36c5af2378358ab11f7b23d33560d3a335c9
fakedarren <darren.waddell@gmail.com>
2011-05-16 21:27:28 +0100

    Trying to write in JavaScript not C#

--- config/configuration.js
+++ config/configuration.js
@@ -1,6 +1,6 @@
 module.exports = {
 	database: process.env['DUOSTACK_DB_MONGODB'] || 'mongodb://localhost/cms',
-	host: process.env['DUOSTACK_DB_MONGODB'] ?? 'http://mootoolscms.duostack.net' : 'http://localhost',
+	host: process.env['DUOSTACK_DB_MONGODB'] ? 'http://mootoolscms.duostack.net' : 'http://localhost',
 	twitter: {
 		consumerKey: '1nUfwvkjGTkketskwt7wKA',
 		consumerSecret: 'TkJWEqxUCQfyU89lSwOw6D8keRp5FALKc7bq1UhoPnE'
