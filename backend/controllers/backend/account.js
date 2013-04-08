3b81de26cd05682ca81e207f5f911ec884d41071
fakedarren <darren.waddell@gmail.com>
2013-04-08 20:07:24 +0100

    Updating login screen / process, can also now log out

--- backend/controllers/backend/account.js
+++ backend/controllers/backend/account.js
@@ -1,12 +1,72 @@
+var config = require('../../config/configuration'),
+    OAuth = require('oauth').OAuth,
+    oa;
+    
+
 module.exports = {
 
     routes: {
+        'logIn': {
+            'url': '/cms/account/login-with-twitter',
+            'method': 'get'
+        },
+        'authenticated': {
+            'url': '/cms/account/authenticated',
+            'method': 'get'
+        },
+        'logOut': {
+            'url': '/cms/account/logout',
+            'method': 'get'
+        },
         'myAccount': {
             'url': '/cms/account/my-account',
             'method': 'get'
         }
     },
     
+    logIn: function(req, res){
+        oa = new OAuth(
+            "https://api.twitter.com/oauth/request_token",
+            "https://api.twitter.com/oauth/access_token",
+            config.twitter.consumerKey,
+            config.twitter.consumerSecret,
+            "1.0A",
+            config.host + '/cms/account/authenticated',
+            "HMAC-SHA1"
+        );
+        req.session.oauth = oa;
+
+        oa.getOAuthRequestToken(function(error, oauth_token, oauth_token_secret, results){
+            if (error){
+                new Error(error.data);
+            } else {
+                req.session.oauth.token = oauth_token;
+                req.session.oauth.token_secret = oauth_token_secret;
+                res.redirect('https://api.twitter.com/oauth/authenticate?oauth_token=' + oauth_token);
+            }
+        }); 
+    },
+    
+    authenticated: function(req, res){
+        if (req.session.oauth){
+            req.session.oauth.verifier = req.query.oauth_verifier;
+            var oauth = req.session.oauth;
+            var callback = function(error, oauth_access_token, oauth_access_token_secret, results){
+                if (error) new Error(error);
+                req.session.user = {
+                    name: results.screen_name
+                };
+                res.redirect("/cms/");
+            };
+            oa.getOAuthAccessToken(oauth.token, oauth.token_secret, oauth.verifier, callback);
+        }
+    },
+
+    logOut: function(req, res){
+        req.session.user = null;
+        res.redirect("/cms/");
+    },
+
     myAccount: function(req, res){
         res.render('backend/account/my-account');
     }
--- backend/controllers/backend/backend.js
+++ backend/controllers/backend/backend.js
@@ -1,10 +1,4 @@
-/*
-Credit: https://github.com/jettro/nodejstryout
-*/
-
-
-var config = require('../../config/configuration'),
-    mongoose = require('mongoose'),
+var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 
@@ -17,10 +11,6 @@ module.exports = {
         },
         'logIn': {
             'url': '/cms/login',
-            'method': 'get'
-        },
-        'authenticated': {
-            'url': '/cms/authenticated',
             'method': 'get'
         },
         'install': {
@@ -40,46 +30,9 @@ module.exports = {
     },
     
     logIn: function(req, res){
-        var sys = require('sys');
-        var OAuth = require('oauth').OAuth;
-
-        oa = new OAuth(
-            "https://api.twitter.com/oauth/request_token",
-            "https://api.twitter.com/oauth/access_token",
-            config.twitter.consumerKey,
-            config.twitter.consumerSecret,
-            "1.0A",
-            config.host + '/cms/authenticated',
-            "HMAC-SHA1"
-        );
-        req.session.oauth = oa;
-
-        oa.getOAuthRequestToken(function(error, oauth_token, oauth_token_secret, results){
-            if (error){
-                new Error(error.data);
-            } else {
-                req.session.oauth.token = oauth_token;
-                req.session.oauth.token_secret = oauth_token_secret;
-                res.redirect('https://api.twitter.com/oauth/authenticate?oauth_token=' + oauth_token);
-            }
-        }); 
+        res.render('backend/account/login');
     },
-    
-    authenticated: function(req, res){
-        if (req.session.oauth){
-            req.session.oauth.verifier = req.query.oauth_verifier;
-            var oauth = req.session.oauth;
-            var callback = function(error, oauth_access_token, oauth_access_token_secret, results){
-                if (error) new Error(error);
-                req.session.user = {
-                    name: results.screen_name
-                };
-                res.redirect("/cms/");
-            };
-            oa.getOAuthAccessToken(oauth.token, oauth.token_secret, oauth.verifier, callback);
-        }
-    },
-    
+  
     install: function(req, res){
         var install = require('../install/install');
         install.forEach(function(module){
